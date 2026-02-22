74a3a1adf92b8bc265cad7061602160826475f32
abner1902 <70659813+abner1902@users.noreply.github.com>
2026-02-22 11:30:31 -0300

    fix: move fonts to src/fonts for next/font/local resolution

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,25 +4,20 @@
     import Navbar from '@/components/Navbar'; // <<-- ESTA LINHA IMPORTA SUA NAVBAR!
 
     const gotham = LocalFont({
-      src: [
-        { path: '../../public/fonts/GothamLight.ttf', weight: '300', style: 'normal' },
-        { path: '../../public/fonts/GothamMedium.ttf', weight: '500', style: 'normal' },
-        { path: '../../public/fonts/GothamBold.ttf', weight: '700', style: 'normal' },
-        { path: '../../public/fonts/GothamBlack.ttf', weight: '900', style: 'normal' },
-        { path: '../../public/fonts/GothamThin.ttf', weight: '100', style: 'normal' },
-        { path: '../../public/fonts/GothamUltra.ttf', weight: '900', style: 'normal' },
-        { path: '../../public/fonts/GothamXLight.ttf', weight: '200', style: 'normal' },
-        { path: '../../public/fonts/GothamBlackItalic.ttf', weight: '900', style: 'italic' },
-        { path: '../../public/fonts/GothamBoldItalic.ttf', weight: '700', style: 'italic' },
-        { path: '../../public/fonts/GothamLightItalic.ttf', weight: '300', style: 'italic' },
-        { path: '../../public/fonts/GothamMediumItalic.ttf', weight: '500', style: 'italic' },
-        { path: '../../public/fonts/GothamThinItalic.ttf', weight: '100', style: 'italic' },
-        { path: '../../public/fonts/GothamUltraItalic.ttf', weight: '900', style: 'italic' },
-        { path: '../../public/fonts/GothamXLightItalic.ttf', weight: '200', style: 'italic' },
-      ],
-      variable: '--font-gotham',
-      display: 'swap',
-    });
+  src: [
+    { path: '../../fonts/GothamLight.ttf', weight: '300', style: 'normal' },
+    { path: '../../fonts/GothamMedium.ttf', weight: '500', style: 'normal' },
+    { path: '../../fonts/GothamBold.ttf', weight: '700', style: 'normal' },
+    { path: '../../fonts/GothamBlack.ttf', weight: '900', style: 'normal' },
+    { path: '../../fonts/GothamThin.ttf', weight: '100', style: 'normal' },
+    { path: '../../fonts/GothamXLight.ttf', weight: '200', style: 'normal' },
+    { path: '../../fonts/GothamBoldItalic.ttf', weight: '700', style: 'italic' },
+    { path: '../../fonts/GothamLightItalic.ttf', weight: '300', style: 'italic' },
+    { path: '../../fonts/GothamMediumItalic.ttf', weight: '500', style: 'italic' },
+  ],
+  variable: '--font-gotham',
+  display: 'swap',
+});
 
     export const metadata = {
       title: "Agya Sounds | Frequências do Terceiro Olho",
